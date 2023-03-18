13ed7f8450f80be93ba7303c8312a87aae557760
Hardbass1991 <midailsorridente@gmail.com>
2023-03-17 21:54:06 -0500

    added preliminary version

---
+++ src/main/java/com/gestion/empleados/servicio/EmpleadoService.java
@@ -0,0 +1,15 @@
+package com.gestion.empleados.servicio;
+
+import com.gestion.empleados.entidades.Empleado;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface EmpleadoService {
+    public List<Empleado> findAll();
+    public Page<Empleado> findAll(Pageable pageable);
+    public void save(Empleado empleado);
+    public Empleado findOne(Long id);
+    public void delete(Long id);
+}
